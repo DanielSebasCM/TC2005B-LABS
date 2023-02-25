ce5b8391c428ad596660298995025da93bc00626
DanielSebasCM <danielsebascm@hotmail.com>
2023-02-25 15:16:06 -0600

    Start Lab12

--- Lab12/index.js
+++ Lab12/index.js
@@ -1,2 +1,23 @@
 import express from "express";
-const dir = __dirname;
+import router from "./routers/index.js";
+import ejs from "ejs";
+
+import { fileURLToPath } from "url";
+import path from "path";
+
+const __filename = fileURLToPath(import.meta.url);
+const __dirname = path.dirname(__filename);
+
+const app = express();
+const port = 5050;
+
+app.set("view engine", "ejs");
+app.set("views", path.join(__dirname, "views"));
+
+app.use(express.json());
+app.use(express.static(path.join(__dirname, "public")));
+app.use(router);
+
+app.listen(port, () => {
+  console.log(`Example app listening at http://localhost:${port}`);
+});
---
+++ Lab12/routers/index.js
@@ -0,0 +1,35 @@
+import express from "express";
+import fs from "fs";
+import { fileURLToPath } from "url";
+import path from "path";
+
+const __filename = fileURLToPath(import.meta.url);
+const __dirname = path.dirname(__filename);
+
+const router = express.Router();
+
+router.get("/", (req, res) => {
+  res.render("index");
+});
+
+router.post("/users", (req, res) => {
+  console.log(req.body);
+
+  const dir = path.join(__dirname, "../public/users/users.txt");
+
+  res.statusCode = 201;
+  if (!fs.existsSync(dir)) {
+    fs.writeFileSync(dir, "Users: \n\n");
+  }
+
+  let contenido = "";
+  contenido += `name: ${req.body.name}\n`;
+  contenido += `lastname: ${req.body.lastname}\n`;
+  contenido += `email: ${req.body.email}\n`;
+  contenido += `age: ${req.body.age}\n\n`;
+
+  fs.appendFileSync(dir, contenido);
+  res.send("User created");
+});
+
+export default router;
